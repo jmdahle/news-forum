da6bd075422c6245823db4345c4b9e24b79d1ef4
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-08-26 22:55:52 -0500

    initial layout complete

--- public/index.js
+++ public/index.js
@@ -1,18 +1,27 @@
 $(document).ready( () => {
     // document events
-    $(document).on('click', '#testArticle', getArticleList );
+    $(document).on('click', '#testArticle', populateArticleList );
     $(document).on('click', '#btn-scrape', submitScrapeRequest );
     $(document).on('click', '#btn-submit-comment', submitComment );
     $(document).on('click', '.btn-open-article-link', openArticle );
     $(document).on('click', '.btn-view-comments', viewComments );
+    $(document).on('click', '#btn-close-comments', closeComments );
 
     //Jquery References
     let $articles = $('#articles-content');
-    let $comments = $('#comments-content');
+    let $comments = $('#comments-wrapper');
     let $message = $('#message-area');
 
-    // document global variables
+    // comments begin hidden
+    hideComments();
 
+    function showComments() {
+        $comments.show();
+    }
+
+    function hideComments() {
+        $comments.hide();
+    }
     function submitScrapeRequest (e) {
         e.preventDefault();
         $
@@ -53,7 +62,7 @@ $(document).ready( () => {
         });
     };
 
-    function getArticleList(e) {
+    function populateArticleList(e) {
         e.preventDefault();
         $
             .get('/articles', dbArticles => {
@@ -84,26 +93,40 @@ $(document).ready( () => {
     }
 
     function viewComments (e) {
+        let article_id = $(this).attr('data-article-id');
+        // updates page references to current/selected Article
+        updateCurrentArticle(article_id);
+        // populate the comment list
+        populateCommentList(article_id);
+        // show the comments
+        showComments();
+    }
+
+    function closeComments (e) {
+        e.preventDefault();
+        hideComments();
+    }
+
+    function updateCurrentArticle(newArticleId) {
         // update old selection
         let selectedArticleId = $('#articleList').attr('data-selected-item');
         $(`#article-${selectedArticleId}`).attr('class','not-selected');
-        // update article-id
-        let article_id = $(this).attr('data-article-id');
         // update new selection
-        $('#articleList').attr('data-selected-item',article_id);
-        $(`#article-${article_id}`).attr('class','selected');
+        $('#articleList').attr('data-selected-item',newArticleId);
+        $(`#article-${newArticleId}`).attr('class','selected');
         // update button data
-        $('#btn-submit-comment').attr('data-id', article_id);
-        // populate Comments for selected Article
-        getCommentList(article_id);
+        $('#btn-submit-comment').attr('data-id', newArticleId);
+        
     }
 
-    function getCommentList(article_id) {
+    function populateCommentList(article_id) {
         $.get(`/comments/${article_id}`, dbComments => {
             $comments.empty();
             let listSource = dbComments;
             let listHtml = '';
             listHtml += '<div>';
+            listHtml += `<button id='btn-close-comments' type='button' class='close' aria-label='Close'> <span aria-hidden='true'>&times;</span></button>`;
+            listHtml += '<h4>Comments</h4>';
             listHtml += '<ul>';
             listSource.forEach(element => {
                 listHtml += '<li>';
@@ -115,6 +138,15 @@ $(document).ready( () => {
             });
             listHtml += '</ul>';
             listHtml += '</div>';
+            listHtml += `<form id='new-comment'>`;
+            listHtml += `<h4>New Comment on Article</h4>`;
+            listHtml += `<label for='new-comment-body'>Comment:</label>`;
+            listHtml += `<textarea id='new-comment-body'></textarea>`;
+            listHtml += `<label for='new-comment-commentor'>Posted By:</label>`;
+            listHtml += `<input type='text' id='new-comment-commentor'>`;
+            listHtml += `<button id='btn-submit-comment' type='submit' ;data-id=''>Post Comment</button>`;
+            listHtml += `</form>`;
+
             listHtml = $(listHtml);
             $comments.append(listHtml);
             });
